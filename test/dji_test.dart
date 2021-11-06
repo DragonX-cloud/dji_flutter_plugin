f70bbeb4891dcfde24dca930f10c586a93984579
Oren Agiv <oa@pnc.co.il>
2021-11-06 21:28:10 +0200

    Added a note to dji_test

--- test/dji_test.dart
+++ test/dji_test.dart
@@ -1,3 +1,6 @@
+// Run this test using:
+// flutter test test/dji_test.dart
+
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:dji/flight.dart';
